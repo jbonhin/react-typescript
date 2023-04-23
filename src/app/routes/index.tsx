ff6beabe9b033019d7f036dbda5710ea707fe53c
João Fernando Bonhin Jr <jbonhin@gmail.com>
2023-04-23 17:00:40 -0300

    feat: Adicionando a Dashboard

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "../pages";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+            <Route path="/pagina-inicial" element={<Dashboard/>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
